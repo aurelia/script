cbf089800d01837515e7e7ca05684cf48c82d45c
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-02-25 11:13:52 +1100

    refactor(quick-start): share code more

--- build/index.js
+++ build/index.js
@@ -43,59 +43,10 @@ initialize();
   };
 })(FrameworkConfiguration.prototype);
 
-/**
- * Bootstrap a new Aurelia instance and start an application
- * @param {QuickStartOptions} options
- * @returns {Aurelia} the running Aurelia instance
- */
-export async function start(options = {}) {
-  const aurelia = new Aurelia();
-  aurelia.use.standardConfiguration();
-  if (options.debug) {
-    aurelia.use.developmentLogging();
-  }
-  if (Array.isArray(options.resources)) {
-    aurelia.use.globalResources(options.resources);
-  }
-  await aurelia.start();
-  await aurelia.setRoot(options.root || 'app.js', options.host || document.body);
-  return aurelia;
-}
-
-/**
- * Bootstrap a new Aurelia instance and start an application by enhancing a DOM tree
- * @param {QuickEnhanceOptions} options Configuration for enhancing a DOM tree
- * @returns {View} the enhanced View by selected options
- */
-export async function enhance(options = {}) {
-  const aurelia = new Aurelia();
-  aurelia.use.standardConfiguration();
-  if (options.debug) {
-    aurelia.use.developmentLogging();
-  }
-  if (Array.isArray(options.resources)) {
-    aurelia.use.globalResources(options.resources);
-  }
-  await aurelia.start();
-  if (typeof options.root === 'function') {
-    options.root = aurelia.container.get(options.root);
-  }
-  return aurelia.enhance(options.root || {}, options.host || document.body);
-}
-
-/** @typedef QuickStartOptions
- * @property {string | Function} [root] application root. Either string or a class, which will be instantiated with DI
- * @property {string | Element} [host] application host, element or a string, which will be used to query the element
- * @property {Array<string | Function>} [resources] global resources for the application
- * @property {boolean} [debug] true to use development console logging
- */
-
-/** @typedef QuickEnhanceOptions
- * @property {{} | Function} [root] binding context for enhancement, can be either object or a class, which will be instantiated with DI
- * @property {string | Element} [host] host node of to be enhanced tree
- * @property {Array<string | Function>} [resources] global resources for the application
- * @property {boolean} [debug] true to use development console logging
- */
+export {
+  start,
+  enhance
+} from './index.quick-start';
 
 export * from 'aurelia-framework';
 export { EventAggregator, includeEventsIn } from 'aurelia-event-aggregator';
