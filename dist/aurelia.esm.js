5e2cf294efee2d82bddae456d9f02fed8397cd29
Rob Eisenberg <rob@bluespire.com>
2019-03-13 19:02:57 -0700

    chore(all): prepare release 1.4.0

--- dist/aurelia.esm.js
+++ dist/aurelia.esm.js
@@ -15297,6 +15297,78 @@ let ConsoleAppender = class ConsoleAppender {
   }
 };
 
+/**
+ * Bootstrap a new Aurelia instance and start an application
+ * @param {QuickStartOptions} options
+ * @returns {Aurelia} the running Aurelia instance
+ */
+const createAndStart = (options = {}) => {
+  const aurelia = new Aurelia();
+  const use = aurelia.use;
+  use.standardConfiguration();
+  if (options.debug) {
+    use.developmentLogging();
+  }
+  if (Array.isArray(options.plugins)) {
+    options.plugins.forEach((plgCfg) => {
+      if (Array.isArray(plgCfg)) {
+        use.plugin(plgCfg[0], plgCfg[1]);
+      } else {
+        use.plugin(plgCfg);
+      }
+    });
+  }
+  if (Array.isArray(options.resources)) {
+    use.globalResources(options.resources);
+  }
+  return aurelia.start();
+};
+
+/**
+ * Bootstrap a new Aurelia instance and start an application
+ * @param {QuickStartOptions} options
+ * @returns {Aurelia} the running Aurelia instance
+ */
+async function start(options = {}) {
+  const aurelia = await createAndStart(options);
+  await aurelia.setRoot(options.root || 'app.js', options.host || document.body);
+  return aurelia;
+}
+
+/**
+ * Bootstrap a new Aurelia instance and start an application by enhancing a DOM tree
+ * @param {QuickEnhanceOptions} options Configuration for enhancing a DOM tree
+ * @returns {View} the enhanced View by selected options
+ */
+async function enhance(options = {}) {
+  const aurelia = await createAndStart(options);
+  if (typeof options.root === 'function') {
+    options.root = aurelia.container.get(options.root);
+  }
+  return aurelia.enhance(options.root || {}, options.host || document.body);
+}
+
+/** @typed ConfigureFn
+ * @param {FrameworkConfiguration} frameWorkConfig
+ * @param {any} plugigConfig
+ */
+
+/** @typedef QuickStartOptions
+ * @property {string | Function} [root] application root. Either string or a class, which will be instantiated with DI
+ * @property {string | Element} [host] application host, element or a string, which will be used to query the element
+ * @property {Array<string | Function>} [resources] global resources for the application
+ * @property {Array<string | {(fwCfg: FrameworkConfiguration) => any}  | [(fwCfg: FrameworkConfiguration, cfg: {}) => any, {}]>} [plugins]
+ * @property {boolean} [debug] true to use development console logging
+ */
+
+/** @typedef QuickEnhanceOptions
+ * @property {{} | Function} [root] binding context for enhancement, can be either object or a class, which will be instantiated with DI
+ * @property {string | Element} [host] host node of to be enhanced tree
+ * @property {Array<string | Function>} [resources] global resources for the application
+ * @property {Array<string | {(fwCfg: FrameworkConfiguration) => any}  | [(fwCfg: FrameworkConfiguration, cfg: {}) => any, {}]>} [plugins]
+ * @property {boolean} [debug] true to use development console logging
+ */
+
 initialize();
 
 // Using static convention to avoid having to fetch / load module dynamically
@@ -15324,11 +15396,11 @@ initialize();
     getLogger('aurelia').error(errorMsg);
     return this;
   };
-
+  
   frameworkCfgProto.router = function() {
     getLogger('aurelia').error(errorMsg);
     return this;
   };
 })(FrameworkConfiguration.prototype);
 
-export { EventAggregator, includeEventsIn, If, Else, Repeat, Compose, Show, Hide, Focus, With, Replaceable, AbstractRepeater, ArrayRepeatStrategy, AttrBindingBehavior, BindingSignaler, DebounceBindingBehavior, FromViewBindingBehavior, HTMLSanitizer, MapRepeatStrategy, NullRepeatStrategy, NumberRepeatStrategy, OneTimeBindingBehavior, OneWayBindingBehavior, RepeatStrategyLocator, SanitizeHTMLValueConverter, SelfBindingBehavior, SetRepeatStrategy, SignalBindingBehavior, ThrottleBindingBehavior, ToViewBindingBehavior, TwoWayBindingBehavior, UpdateTriggerBindingBehavior, createFullOverrideContext, updateOneTimeBinding, updateOverrideContext, isOneTime, viewsRequireLifecycle, unwrapExpression, getItemsSourceExpression, Aurelia, FrameworkConfiguration, LogManager, resolver, StrategyResolver, Lazy, All, Optional, Parent, Factory, NewInstance, getDecoratorDependencies, lazy, all, optional, parent, factory, newInstance, invoker, invokeAsFactory, FactoryInvoker, registration, transient, singleton, TransientRegistration, SingletonRegistration, _emptyParameters, InvocationHandler, Container, autoinject, inject, targetContext, sourceContext, camelCase, createOverrideContext, getContextFor, createScopeForTest, connectable, enqueueBindingConnect, setConnectQueueThreshold, enableConnectQueue, disableConnectQueue, getConnectQueueSize, subscriberCollection, ExpressionObserver, calcSplices, mergeSplice, projectArraySplices, getChangeRecords, ModifyCollectionObserver, CollectionLengthObserver, getArrayObserver, Expression, BindingBehavior, ValueConverter, Assign, Conditional, AccessThis, AccessScope, AccessMember, AccessKeyed, CallScope, CallMember, CallFunction, Binary, Unary, LiteralPrimitive, LiteralString, LiteralTemplate, LiteralArray, LiteralObject, Unparser, ExpressionCloner, cloneExpression, bindingMode, Parser, ParserImplementation, getMapObserver, delegationStrategy, EventManager, EventSubscriber, DirtyChecker, DirtyCheckProperty, propertyAccessor, PrimitiveObserver, SetterObserver, XLinkAttributeObserver, dataAttributeAccessor, DataAttributeObserver, StyleObserver, ValueAttributeObserver, CheckedObserver, SelectValueObserver, ClassObserver, hasDeclaredDependencies, declarePropertyDependencies, computedFrom, ComputedExpression, createComputedObserver, elements, presentationElements, presentationAttributes, SVGAnalyzer, ObserverLocator, ObjectObservationAdapter, BindingExpression, Binding, CallExpression, Call, ValueConverterResource, valueConverter, BindingBehaviorResource, bindingBehavior, ListenerExpression, Listener, NameExpression, BindingEngine, getSetObserver, observable, connectBindingToSignal, signalBindings, metadata, Origin, decorators, deprecated, mixin, protocol, animationEvent, Animator, CompositionTransactionNotifier, CompositionTransactionOwnershipToken, CompositionTransaction, _hyphenate, _isAllWhitespace, ViewEngineHooksResource, viewEngineHooks, ElementEvents, ResourceLoadContext, ViewCompileInstruction, BehaviorInstruction, TargetInstruction, viewStrategy, RelativeViewStrategy, ConventionalViewStrategy, NoViewStrategy, TemplateRegistryViewStrategy, InlineViewStrategy, StaticViewStrategy, ViewLocator, BindingLanguage, SlotCustomAttribute, PassThroughSlot, ShadowSlot, ShadowDOM, validateBehaviorName, ViewResources, View, ViewSlot, BoundViewFactory, ViewFactory, ViewCompiler, ResourceModule, ResourceDescription, ModuleAnalyzer, ViewEngine, Controller, BehaviorPropertyObserver, BindableProperty, HtmlBehaviorResource, children, child, SwapStrategies, CompositionEngine, ElementConfigResource, resource, behavior, customElement, customAttribute, templateController, bindable, dynamicOptions, useShadowDOM, processAttributes, processContent, containerless, useViewStrategy, useView, inlineView, noView, view, elementConfig, viewResources, TemplatingEngine, TemplateDependency, TemplateRegistryEntry, Loader, TaskQueue, relativeToFile, join, buildQueryString, parseQueryString, AggregateError, FEATURE, PLATFORM, DOM, isInitialized, initializePAL, reset };
+export { start, enhance, EventAggregator, includeEventsIn, If, Else, Repeat, Compose, Show, Hide, Focus, With, Replaceable, AbstractRepeater, ArrayRepeatStrategy, AttrBindingBehavior, BindingSignaler, DebounceBindingBehavior, FromViewBindingBehavior, HTMLSanitizer, MapRepeatStrategy, NullRepeatStrategy, NumberRepeatStrategy, OneTimeBindingBehavior, OneWayBindingBehavior, RepeatStrategyLocator, SanitizeHTMLValueConverter, SelfBindingBehavior, SetRepeatStrategy, SignalBindingBehavior, ThrottleBindingBehavior, ToViewBindingBehavior, TwoWayBindingBehavior, UpdateTriggerBindingBehavior, createFullOverrideContext, updateOneTimeBinding, updateOverrideContext, isOneTime, viewsRequireLifecycle, unwrapExpression, getItemsSourceExpression, Aurelia, FrameworkConfiguration, LogManager, resolver, StrategyResolver, Lazy, All, Optional, Parent, Factory, NewInstance, getDecoratorDependencies, lazy, all, optional, parent, factory, newInstance, invoker, invokeAsFactory, FactoryInvoker, registration, transient, singleton, TransientRegistration, SingletonRegistration, _emptyParameters, InvocationHandler, Container, autoinject, inject, targetContext, sourceContext, camelCase, createOverrideContext, getContextFor, createScopeForTest, connectable, enqueueBindingConnect, setConnectQueueThreshold, enableConnectQueue, disableConnectQueue, getConnectQueueSize, subscriberCollection, ExpressionObserver, calcSplices, mergeSplice, projectArraySplices, getChangeRecords, ModifyCollectionObserver, CollectionLengthObserver, getArrayObserver, Expression, BindingBehavior, ValueConverter, Assign, Conditional, AccessThis, AccessScope, AccessMember, AccessKeyed, CallScope, CallMember, CallFunction, Binary, Unary, LiteralPrimitive, LiteralString, LiteralTemplate, LiteralArray, LiteralObject, Unparser, ExpressionCloner, cloneExpression, bindingMode, Parser, ParserImplementation, getMapObserver, delegationStrategy, EventManager, EventSubscriber, DirtyChecker, DirtyCheckProperty, propertyAccessor, PrimitiveObserver, SetterObserver, XLinkAttributeObserver, dataAttributeAccessor, DataAttributeObserver, StyleObserver, ValueAttributeObserver, CheckedObserver, SelectValueObserver, ClassObserver, hasDeclaredDependencies, declarePropertyDependencies, computedFrom, ComputedExpression, createComputedObserver, elements, presentationElements, presentationAttributes, SVGAnalyzer, ObserverLocator, ObjectObservationAdapter, BindingExpression, Binding, CallExpression, Call, ValueConverterResource, valueConverter, BindingBehaviorResource, bindingBehavior, ListenerExpression, Listener, NameExpression, BindingEngine, getSetObserver, observable, connectBindingToSignal, signalBindings, metadata, Origin, decorators, deprecated, mixin, protocol, animationEvent, Animator, CompositionTransactionNotifier, CompositionTransactionOwnershipToken, CompositionTransaction, _hyphenate, _isAllWhitespace, ViewEngineHooksResource, viewEngineHooks, ElementEvents, ResourceLoadContext, ViewCompileInstruction, BehaviorInstruction, TargetInstruction, viewStrategy, RelativeViewStrategy, ConventionalViewStrategy, NoViewStrategy, TemplateRegistryViewStrategy, InlineViewStrategy, StaticViewStrategy, ViewLocator, BindingLanguage, SlotCustomAttribute, PassThroughSlot, ShadowSlot, ShadowDOM, validateBehaviorName, ViewResources, View, ViewSlot, BoundViewFactory, ViewFactory, ViewCompiler, ResourceModule, ResourceDescription, ModuleAnalyzer, ViewEngine, Controller, BehaviorPropertyObserver, BindableProperty, HtmlBehaviorResource, children, child, SwapStrategies, CompositionEngine, ElementConfigResource, resource, behavior, customElement, customAttribute, templateController, bindable, dynamicOptions, useShadowDOM, processAttributes, processContent, containerless, useViewStrategy, useView, inlineView, noView, view, elementConfig, viewResources, TemplatingEngine, TemplateDependency, TemplateRegistryEntry, Loader, TaskQueue, relativeToFile, join, buildQueryString, parseQueryString, AggregateError, FEATURE, PLATFORM, DOM, isInitialized, initializePAL, reset };
