a3e9948a417fa930fb4682d269017601cb2eb35d
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-05-02 23:02:07 +1000

    chore(dist): add rollup to build es5 umd dist

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default [
   {
@@ -83,5 +84,87 @@ export default [
         // ignoreGlobal: true
       })
     ]
-  }
+  },
+  {
+    input: 'build/index.no-loader.js',
+    output: [
+      {
+        file: 'dist/aurelia_no_loader.es5.umd.js',
+        format: 'umd',
+        name: 'au'
+      }
+    ],
+
+    // external: externalLibs,
+    plugins: [
+      replace({
+        FEATURE_NO_ES2015: true,
+        FEATURE_NO_ES2016: true,
+        // Need for Reflect metadata
+        FEATURE_NO_ESNEXT: undefined,
+        FEATURE_NO_IE: true,
+        FEATURE_NO_UNPARSER: true,
+        FEATURE_ROUTER: process.env.ROUTER === false ? undefined : true
+      }),
+
+      nodeResolve({
+        // jsnext: true,
+        // main: true
+      }),
+
+      commonjs({
+        // ignoreGlobal: true
+      }),
+
+      babel({
+        presets: [
+          [
+            '@babel/preset-env',
+            { loose: true }
+          ]
+        ]
+      })
+    ]
+  },
+  {
+    input: 'build/index.no-loader.full.js',
+    output: [
+      {
+        file: 'dist/aurelia_router_no_loader.es5.umd.js',
+        format: 'umd',
+        name: 'au'
+      }
+    ],
+
+    // external: externalLibs,
+    plugins: [
+      replace({
+        FEATURE_NO_ES2015: true,
+        FEATURE_NO_ES2016: true,
+        // Need for Reflect metadata
+        FEATURE_NO_ESNEXT: undefined,
+        FEATURE_NO_IE: true,
+        FEATURE_NO_UNPARSER: true,
+        FEATURE_ROUTER: process.env.ROUTER === false ? undefined : true
+      }),
+
+      nodeResolve({
+        // jsnext: true,
+        // main: true
+      }),
+
+      commonjs({
+        // ignoreGlobal: true
+      }),
+
+      babel({
+        presets: [
+          [
+            '@babel/preset-env',
+            { loose: true }
+          ]
+        ]
+      })
+    ]
+  },
 ]
