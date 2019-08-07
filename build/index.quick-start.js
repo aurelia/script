be20ce4ebbc9300ebdb3c7bc6d8aae5ebc42d2f4
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-08-07 21:16:52 +1000

    fix(shortcut-fn): don't use async fn, fix version in readme

--- build/index.quick-start.js
+++ build/index.quick-start.js
@@ -32,10 +32,9 @@ const createAndStart = (options = {}) => {
  * @param {QuickStartOptions} options
  * @returns {Aurelia} the running Aurelia instance
  */
-export async function start(options = {}) {
-  const aurelia = await createAndStart(options);
-  await aurelia.setRoot(options.root || 'app.js', options.host || document.body);
-  return aurelia;
+export function start(options = {}) {
+  return createAndStart(options)
+    .then(aurelia => aurelia.setRoot(options.root || 'app.js', options.host || document.body));
 }
 
 /**
@@ -43,12 +42,14 @@ export async function start(options = {}) {
  * @param {QuickEnhanceOptions} options Configuration for enhancing a DOM tree
  * @returns {View} the enhanced View by selected options
  */
-export async function enhance(options = {}) {
-  const aurelia = await createAndStart(options);
-  if (typeof options.root === 'function') {
-    options.root = aurelia.container.get(options.root);
-  }
-  return aurelia.enhance(options.root || {}, options.host || document.body);
+export function enhance(options = {}) {
+  return createAndStart(options)
+    .then(aurelia => {
+      if (typeof options.root === 'function') {
+        options.root = aurelia.container.get(options.root);
+      }
+      return aurelia.enhance(options.root || {}, options.host || document.body);
+    });
 }
 
 /** @typed ConfigureFn
